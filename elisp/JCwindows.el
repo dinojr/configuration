9628b4e4229fdf35b5a3dc7e63872aba77906199
Julien Cubizolles <j.cubizolles@free.fr>
2014-09-09 16:55:06 +0200

    changement de win-switch pour switch-window

--- elisp/JCwindows.el
+++ elisp/JCwindows.el
@@ -1,5 +1,7 @@
 ;; ~/.emacs.d/JCwindows.el -*- mode: lisp-*-
 
-(require 'win-switch)
-(win-switch-setup-keys-ijkl "\C-xo")
-(setq win-switch-idle-time 1.5)
+;; (require 'win-switch)
+;; (win-switch-setup-keys-ijkl "\C-xo")
+;; (setq win-switch-idle-time 1.5)
+
+(global-set-key (kbd "C-x o") 'switch-window)
