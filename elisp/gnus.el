5f88933556eb962f42238cf00bbc1890128511f1
Julien Cubizolles <j.cubizolles@free.fr>
2014-10-06 14:27:48 +0200

    Modification de la date dans gnus-user-date-format-alist

--- elisp/gnus.el
+++ elisp/gnus.el
@@ -373,7 +373,8 @@
 (defun oxy-unicode-threads () 
   (interactive)
   (setq gnus-summary-dummy-line-format "        %8{│%}    %8{│%}%O   %(%8{│%}                       %7{│%}%) %6{□%}  %S\n"
-	gnus-summary-line-format "%8{%8d│%}%8{%4k│%}%O%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{│%} %6{%B%} %s\n"
+	;; gnus-summary-line-format "%8{%8d│%}%8{%4k│%}%O%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{│%} %6{%B%} %s\n"
+	gnus-summary-line-format "%8{%8&user-date;│%}%8{%4k│%}%O%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{│%} %6{%B%} %s\n"
 	gnus-sum-thread-tree-indent " "
 	gnus-sum-thread-tree-root "■ "
 	gnus-sum-thread-tree-false-root "□ "
@@ -384,7 +385,8 @@
 
 (defun oxy-unicode-threads-heavy () 
   (interactive)
-  (setq gnus-summary-line-format "%8{%8d│%}%8{%4k│%}%O%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{║%} %6{%B%} %s\n"
+  (setq ;; gnus-summary-line-format "%8{%8d│%}%8{%4k│%}%O%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{║%} %6{%B%} %s\n"
+        gnus-summary-line-format "%8{%12&user-date;│%}%8{%4k│%}%O%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{║%} %6{%B%} %s\n"
 	gnus-summary-dummy-line-format "        %8{│%}    %8{│%}%O   %(%8{│%}                       %7{║%}%) %6{┏○%}  %S\n"
 	gnus-sum-thread-tree-indent " "
 	gnus-sum-thread-tree-root "┏● " 
@@ -395,6 +397,7 @@
 	gnus-sum-thread-tree-single-leaf "┗━━❯ "))
 
 (oxy-unicode-threads-heavy)
+;; (oxy-unicode-threads)
 
 ;(setq gnus-ignored-from-addresses (quote ("Julien Cubizolles" "j.cubizolles@free.fr")))
 (setq gnus-ignored-from-addresses "j.cubizolles@free.fr")
