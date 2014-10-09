a1aa92a773328024a8dc4b2fc86bbc3f7ec3c735
Julien Cubizolles <j.cubizolles@free.fr>
2014-10-09 21:34:13 +0200

    bug sur summary line format

--- elisp/gnus.el
+++ elisp/gnus.el
@@ -387,7 +387,7 @@
   (interactive)
   (setq ;; gnus-summary-line-format "%8{%8d│%}%8{%4k│%}%O%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{║%} %6{%B%} %s\n"
         gnus-summary-line-format "%8{%11&user-date;│%}%8{%4k│%}%O%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{║%} %6{%B%} %s\n"
-	gnus-summary-dummy-line-format "        %8{│%}       %8{│%}%O   %(%8{│%}                       %7{║%}%) %6{┏○%}  %S\n"
+	gnus-summary-dummy-line-format "           %8{│%}    %8{│%}%O   %(%8{│%}                       %7{║%}%) %6{┏○%}  %S\n"
 	gnus-sum-thread-tree-indent " "
 	gnus-sum-thread-tree-root "┏● " 
 	gnus-sum-thread-tree-false-root " ○ "
