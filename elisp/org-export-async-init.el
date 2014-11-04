ea69142e9054ba2872de2f7e0bd037e886ede1f0
Julien Cubizolles <j.cubizolles@free.fr>
2014-11-04 15:28:31 +0100

    * elisp/org-export-async-init.el: l'export async utilise le dépôt github

--- elisp/org-export-async-init.el
+++ elisp/org-export-async-init.el
@@ -1,10 +1,15 @@
 ;;~/.emacs -*- mode: lisp-*-
 
-;; org est installé avec melpa
-(require 'package)
-(package-initialize)
+;; si org est installé avec melpa
+;; (require 'package)
+;; (package-initialize)
 
+;; si org est installé avec git
+(add-to-list 'load-path (expand-file-name "~/info/emacs/org-mode/lisp"))
+(add-to-list 'load-path (expand-file-name  "~/info/emacs/org-mode/contrib/lisp"))
+
+(require 'org)
+(require 'ox)
 (add-to-list 'load-path "~/configuration/elisp/")
 (load "JCorg.el")
-(require 'ox)
 (load "JClatex.el")
