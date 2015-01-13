76034fc1180887537c08a241e19c3789a877dd7c
Julien Cubizolles <j.cubizolles@free.fr>
2015-01-13 11:26:44 +0100

    * elisp/JCworkgroups2.el: Premier commit

---
+++ elisp/JCworkgroups2.el
@@ -0,0 +1,15 @@
+;; ~/.emacs.d/JCworkgroups2.el
+
+(require 'workgroups2)
+
+(setq wg-morph-on nil)
+
+;; (setq wg-morph-vsteps 3)
+;; (setq wg-morph-hsteps 3)
+
+;; C-c w est le standard, mais déjà utilisé pour browse-url
+(setq wg-prefix-key [f1])
+
+(workgroups-mode 1)
+
+
