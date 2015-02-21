7e7f527d4e86d28f1e0472ee1b5bb251f44d2b49
Julien Cubizolles <j.cubizolles@free.fr>
2015-02-21 22:51:02 +0100

    * elisp/JChelm.el: ajout des raccourcis principaux

--- elisp/JChelm.el
+++ elisp/JChelm.el
@@ -10,6 +10,26 @@
 (require 'helm-files)
 (require 'helm-grep)
 
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+(helm-autoresize-mode t)
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+
+(global-set-key (kbd "C-x b") 'helm-mini)
+
+
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
+
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+(global-set-key (kbd "C-c h o") 'helm-occur)
+
 ;; (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
 ;; (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 ;; (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
