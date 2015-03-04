50d6dcfcad15574f431080f45467b8b37e9d37a9
Julien Cubizolles <j.cubizolles@free.fr>
2015-03-04 13:02:32 +0100

    * elisp/JClpr.el: premier commit

---
+++ elisp/JClpr.el
@@ -0,0 +1,13 @@
+;; ~/.emacs.d/JClpr.el -*- mode: lisp-*-
+
+
+;; file where things will be saved
+;; (setq bbdb-file "~/.emacs.d/bbdb")
+
+(require 'printing)
+
+(setq printer-name "laserjet")
+
+(setq lpr-command "lpr")
+
+(setq lpr-switches '("-o landscape -o fit-to-page -o sides=two-sided-short-edge -o page-ranges=1-"))
