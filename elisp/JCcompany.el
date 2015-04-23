6b8b9dacd42f008c95cac1a8f0d73f6e97772ca1
Julien Cubizolles <j.cubizolles@free.fr>
2015-04-23 08:04:48 +0200

    installation de company, modes auctex, math, org et helm

---
+++ elisp/JCcompany.el
@@ -0,0 +1,18 @@
+;; ~/.emacs.d/JCcompany.el -*- mode: lisp-*-
+
+(eval-after-load 'company
+  '(progn
+     (define-key company-mode-map (kbd "C-:") 'helm-company)
+     (define-key company-active-map (kbd "C-:") 'helm-company)))
+
+(add-hook 'after-init-hook 'global-company-mode)
+(company-auctex-init)
+
+;; local configuration for TeX modes
+(defun jc-company-latex-mode-setup ()
+  (setq-local company-backends
+              (append '(company-math-symbols-latex company-latex-commands company-math-symbols-unicode)
+                      company-backends)))
+
+(add-hook 'LaTeX-mode-hook 'jc-company-latex-mode-setup)
+(add-hook 'org-mode-hook 'jc-company-latex-mode-setup)
