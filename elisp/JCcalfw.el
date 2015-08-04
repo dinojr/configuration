0a29b82a858fa959955290e5700ec86a8840c3ec
Julien Cubizolles <j.cubizolles@free.fr>
2015-08-04 07:54:59 +0200

    Ajout de JCcalfw.el

---
+++ elisp/JCcalfw.el
@@ -0,0 +1,15 @@
+;; ~/.emacs.d/JCcalfw.el -*- mode: lisp-*-
+
+(require 'calfw)
+(require 'calfw-org)
+(require 'calfw-ical)
+(setq cfw:fchar-junction ?╋
+      cfw:fchar-vertical-line ?┃
+      cfw:fchar-horizontal-line ?━
+      cfw:fchar-left-junction ?┣
+      cfw:fchar-right-junction ?┫
+      cfw:fchar-top-junction ?┯
+      cfw:fchar-top-left-corner ?┏
+      cfw:fchar-top-right-corner ?┓)
+
+;; (cfw:open-ical-calendar "https://www.google.com/calendar/ical/fh0o4m68o252290ovr7voe3584%40group.calendar.google.com/private-7bebfd7102c41303b1e527c13e1c9b54/basic.ics")
