81d6d5c069cb982714b02616379483b27dadf120
Julien Cubizolles <j.cubizolles@free.fr>
2015-08-15 19:54:53 +0200

    my-anniversaires
    pour afficher les anniversaires à venir

--- elisp/JCbbdb.el
+++ elisp/JCbbdb.el
@@ -8,9 +8,6 @@
 
 (add-to-list 'load-path "/home/wilk/.emacs.d/bbdb-vcard")
 
-
-
-
 (bbdb-initialize 'gnus 'message)
 (bbdb-mua-auto-update-init 'gnus 'message)
 
@@ -19,10 +16,51 @@
 
 (setq bbdb-mua-update-interactive-p '(query . create))
 
+(require 'org-bbdb)
+
+;;; Return list of anniversaries for today and the next n (default: 7) days.
+;;; This is meant to be used in an org file instead of org-bbdb-anniversaries:
+;;;
+;;; %%(my-anniversaries)
+;;;
+;;; or
+;;; %%(my-anniversaries 3)
+;;;
+;;; to override the 7-day default.
+
+ (defun date-list (date &optional n)
+   "Return a list of dates in (m d y) format from 'date' to n days hence."
+   (if (not n) (setq n 7))
+   (let ((abs (calendar-absolute-from-gregorian date))
+         (i 0)
+         ret)
+     (while (<= i n)
+       (setq ret (cons (calendar-gregorian-from-absolute (+ abs i)) ret))
+       (setq i (1+ i)))
+     (reverse ret)))
+
+ (defun annotate-link-with-date (d l)
+   "Annotate text of each element of l with the anniversary date.
+   The assumption is that the text is a bbdb link  of the form
+   [[bbdb:name][Description]] and the annotation
+   is added to the description."
+   (let ((f (lambda (x)
+              (string-match "]]" x)
+              (replace-match (format " -- %d-%d-%d\\&" (caddr d) (car d) (cadr d)) nil nil x))))
+     (mapcar f l)))
+
+ (defun my-anniversaries (&optional n)
+   "Return list of anniversaries for today and the next n days (default 7).
+ 'date' is dynamically bound."
+   (let ((dates (date-list date n))
+         (f (lambda (d) (let ((date d)) (annotate-link-with-date d (org-bbdb-anniversaries))))))
+     (delete nil (mapcan f dates))))
+
+(setq org-bbdb-anniversary-field 'birthday)
+
 ;; Raccourcis
 
 (define-key bbdb-mode-map "r" 'bbdb-merge-records)
-
 
 ;; (defun remove-field-value (field value)
 ;;   "From every record in the active bbdb database, remove the notes field whose
