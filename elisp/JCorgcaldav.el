a857df9b48c18462170f78782a271e6cf1faf2c4
Julien Cubizolles <j.cubizolles@free.fr>
2015-08-28 16:32:10 +0200

    org-caldav-save-directory changed to ~/org/
    to ensure synchronization by unison

--- elisp/JCorgcaldav.el
+++ elisp/JCorgcaldav.el
@@ -5,5 +5,18 @@
 (setq org-caldav-inbox "~/org/org-caldav.org")
 (setq org-caldav-files '("~/org/orgfiles/planning.org"))
 (setq org-icalendar-timezone "Europe/Paris")
+(setq org-caldav-save-directory "~/org/") ; to ensure synchronization by unison
+;; Fonction pour créer plein d'entrées 
+;; #+begin_src emacs-lisp :results raw
+;; (with-temp-buffer
+;; (insert "*** TP
+;; <2015-09-04 ven 08:00-12:00>")
+;; (goto-char (point-min))
+;; (org-clone-subtree-with-time-shift 30 "+1w")
+;; ;; ignore this
+;; (let ((str (buffer-string)))
+;; (set-text-properties 0 (length str) nil str) str))
+;; #+end_src
+;; #+RESULTS:
 
-(org-caldav-sync)
+;; (org-caldav-sync)
