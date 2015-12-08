ee71793349914c69432326688be2f9d65660196e
Julien Cubizolles <j.cubizolles@free.fr>
2015-12-08 11:22:36 +0100

    Back to git org-mode
    The completion-function is now fixed

--- elisp/JCorg.el
+++ elisp/JCorg.el
@@ -1,8 +1,8 @@
 ;; ~/emacs.d/JCorg.el -*- mode: lisp-*-
 
 
-;; (add-to-list 'load-path "~/info/emacs/org-mode/lisp")
-;; (add-to-list 'load-path "~/info/emacs/org-mode/contrib/lisp")
+(add-to-list 'load-path "~/info/emacs/org-mode/lisp")
+(add-to-list 'load-path "~/info/emacs/org-mode/contrib/lisp")
 (require 'org)
 (require 'cl)
 
