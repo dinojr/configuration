8a16044b79a6c67d20ca40f2679dfdbd1672e557
Julien Cubizolles <j.cubizolles@free.fr>
2015-12-09 10:51:28 +0100

    auctex-extra loaded

--- elisp/JClatex.el
+++ elisp/JClatex.el
@@ -1,9 +1,10 @@
 ;; ~/.emacs.d/JClatex.el -*- mode: lisp-*-
 
-;; (add-to-list 'load-path "~/info/emacs/auctex")
-;; (setq TeX-data-directory "~/info/emacs/auctex/")
-;; (add-to-list 'load-path "~/info/emacs/auctex/")
-;; (add-to-list 'load-path "~/info/emacs/auctex/preview/")
+(add-to-list 'load-path "~/info/emacs/auctex")
+(setq TeX-data-directory "~/info/emacs/auctex/")
+(add-to-list 'load-path "~/info/emacs/auctex/")
+(add-to-list 'load-path "~/info/emacs/auctex/preview/")
+
 (require 'package)
 (package-initialize)
 (require 'tex)
@@ -29,6 +30,8 @@
       ;; crochets automatiques pour exposants et indices
       (setq TeX-electric-sub-and-superscript t)
 )))
+
+(add-hook 'LaTeX-mode-hook #'latex-extra-mode)
 
 (setq LaTeX-section-hook
       '(LaTeX-section-title
