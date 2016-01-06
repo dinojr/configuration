8516a636312f6bf362ba99a552b80eb4d7aa01d3
Julien Cubizolles <j.cubizolles@free.fr>
2016-01-06 01:05:45 +0100

    jc-replace-regexp and friends don't lose region

--- elisp/JCregexps.el
+++ elisp/JCregexps.el
@@ -1,12 +1,20 @@
 ;; ~/.emacs.d/JCregexps.el -*- mode: lisp-*-
 
+;; (defun jc-replace-regexp-in-region (Begin End from to)
+;;   "Replace from by to in the selected region"
+;;   (interactive "r")
+;;   (let ((deactivate-mark nil))
+;;     (query-replace-regexp from to nil Begin End)
+;;     ;; (setq deactivate-mark nil)
+;;     ))
+
 (defun jc-replace-regexp-in-region (Begin End from to)
-  "Replace from by to in the selected region"
-  ;; (interactive "r")
+  (interactive "r")
   (save-excursion
-    (let (deactivate-mark)
-      (query-replace-regexp from to nil Begin End)))
-  )
+    (let ((deactivate-mark nil))
+      (goto-char (min Begin End))
+      (while (re-search-forward from nil t)
+	(replace-match to nil nil)))))
 
 (defun jc-dollar-to-paren (Begin End)
   "Replace $toto$ by \\(toto\\) in selected region"
@@ -63,7 +71,15 @@
   (jc-replace-regexp-in-region Begin End "\\\\only<\\(.*?\\)>{\\([ \n \t [:ascii:] [:nonascii:]]*\\)}" "* \?\n\t:PROPERTIES:\n\t:BEAMER_env: action\n\t:BEAMER_ACT: only@\\1\n\t:END:\n\n\t\\2")
   )
 
-(defun jc-do-all-regexps ()
+(defun jc-do-all-regexps (Begin End)
+  (interactive "r")
+  (let ((deactivate-mark nil))
+    (jc-alert-or-emph-to-stars Begin End)
+    (jc-dollar-to-paren Begin End)
+    (jc-item-to-plus Begin End)
+    (jc-unit-to-si Begin End))
+  )
+
   (interactive)
   (lambda ()
     (jc-alert-or-to-stars)
