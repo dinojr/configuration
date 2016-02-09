b6ec22f1a03b5dfb7396657df1ea47143680a35e
Julien Cubizolles <j.cubizolles@free.fr>
2016-02-09 09:45:04 +0100

    ace-link configuration

---
+++ elisp/JCace-link.el
@@ -0,0 +1,8 @@
+;; ~/.emacs.d/JCacelink.el -*- mode: lisp-*-
+
+(ace-link-setup-default)
+
+(define-key org-mode-map (kbd "M-o") 'ace-link-org)
+
+(define-key gnus-summary-mode-map (kbd "M-o") 'ace-link-gnus)
+(define-key gnus-article-mode-map (kbd "M-o") 'ace-link-gnus)
