e7a6603241fb5a3e3100930aecc2d66be2c46832
Julien Cubizolles <j.cubizolles@free.fr>
2016-02-10 13:57:03 +0100

    correct syntax to add pdfview to org-file-apps from now on

--- elisp/JCpdftools.el
+++ elisp/JCpdftools.el
@@ -2,7 +2,11 @@
 
 (pdf-tools-install)
 (eval-after-load 'org '(progn (require 'org-pdfview)
-			      (add-to-list 'org-file-apps '("\\.pdf\\'" . org-pdfview-open))
-			      (add-to-list 'org-file-apps '("\\.pdf::\\([[:digit:]]+\\)\\'" . org-pdfview-open))))
+			      (add-to-list 'org-file-apps '("\\.pdf\\'" . (lambda (file link) (org-pdfview-open file))))
+			      (add-to-list 'org-file-apps '("\\.pdf::\\([[:digit:]]+\\)\\'" . (lambda (file link) (org-pdfview-open file))))
+			      ))
+			      
 
 (setq revert-without-query (quote (".*.pdf")))
+
+
