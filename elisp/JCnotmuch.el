72478cfcc4fda59ed87529df11350838bf5e7154
Julien Cubizolles <j.cubizolles@free.fr>
2016-02-17 13:52:29 +0100

    jc-notmuch-goto-message-in-gnus
    Goes from a notmuch search to the message in Gnus

--- elisp/JCnotmuch.el
+++ elisp/JCnotmuch.el
@@ -1,31 +1,37 @@
 ;; ~/.emacs.d/JCnotmuch.el -*- mode: lisp-*-
 (require 'notmuch)
-;; (defun jc-notmuch-file-to-group (file)
-;;   "Calculate the Gnus group name from the given file name.
-;;   "
-;;   (let ((group (file-name-directory (directory-file-name (file-name-directory file)))))
-;;     ;; (setq group (replace-regexp-in-string ".*/Maildir/" "nnimap+Maildir:" group))
-;;     ;; (setq group (replace-regexp-in-string "/$" "" group))
-;;     ;; (if (string-match ":$" group)
-;;     ;;     (concat group "INBOX")
-;;     ;;   (replace-regexp-in-string ":\\." ":" group))
-;;     (if (string-match "Local" group)
-;; 	(replace-regexp-in-string "/home/wilk/email/Local/" "" group))
-;;     )
-;;   )
+(require 'org-gnus)
 
-;; (defun jc-notmuch-goto-message-in-gnus ()
-;;   "Open a summary buffer containing the current notmuch
-;;   article."
-;;   (interactive)
-;;   (let ((group (jc-notmuch-file-to-group (notmuch-show-get-filename)))
-;;         (message-id (replace-regexp-in-string
-;;                      "^id:" "" (notmuch-show-get-message-id))))
-;;     (setq message-id (replace-regexp-in-string "\"" "" message-id))
-;;     (if (and group message-id)
-;;         (progn 
-;;           (switch-to-buffer "*Group*")
-;;           (org-gnus-follow-link group message-id))
-;;       (message "Couldn't get relevant infos for switching to Gnus."))))
+(defun jc-notmuch-file-to-group (file)
+  (let ((group (directory-file-name (file-name-directory file))))
+    (if (string-match "Local" group)
+	(replace-regexp-in-string "/home/wilk/email/Local/" "" group)
+      (let ((group (directory-file-name (file-name-directory group))))
+	(setq group (replace-regexp-in-string ".*/Maildir/\\(Gmail\\|Free\\)/" "nnimap+\\1Offline:" group))
+	(setq group (replace-regexp-in-string "/$" "" group))
+	(if (string-match ":$" group)
+	    (concat group "INBOX")
+	  (replace-regexp-in-string ":\\." ":" group)))
+      )
+    ))
 
-;; (define-key notmuch-show-mode-map (kbd "C-c C-c") 'jc-notmuch-goto-message-in-gnus)
+(defun jc-notmuch-goto-message-in-gnus ()
+  "Open a summary buffer containing the current notmuch
+  article."
+  (interactive)
+  (let ((group (jc-notmuch-file-to-group (notmuch-show-get-filename)))
+        (message-id (replace-regexp-in-string
+                     "^id:" "" (notmuch-show-get-message-id))))
+    (setq message-id (replace-regexp-in-string "\"" "" message-id))
+    (if (and group message-id)
+        (progn 
+          (switch-to-buffer "*Group*")
+          (org-gnus-follow-link group message-id))
+      (message "Couldn't get relevant infos for switching to Gnus."))))
+
+(add-hook 'gnus-group-mode-hook 'jc-notmuch-shortcut)
+(defun jc-notmuch-shortcut ()
+  (define-key gnus-group-mode-map "Gg" 'notmuch-search)
+  )
+
+(define-key notmuch-show-mode-map (kbd "C-c C-c") 'jc-notmuch-goto-message-in-gnus)
