8fd9e3694b77af51d9a07b7043c7a52ec4f93424
Julien Cubizolles <j.cubizolles@free.fr>
2016-03-04 16:40:25 +0100

    some trials to get mpd->emms working for ogg and flac

--- elisp/JCemms.el
+++ elisp/JCemms.el
@@ -1,25 +1,37 @@
 ;; ~/.emacs.d/JCemms.el -*- mode: lisp-*-
 
 (require 'emms-setup)
+(require 'emms-browser)
 (emms-standard)
 (emms-default-players)
+(emms-cache 1)
+(setq emms-cache-file "~/.emacs.d/emms/cache")
 ;; (setq emms-source-file-directory-tree-function 'emms-source-file-directory-tree-find)
 
 (require 'emms-player-mpd)
 (add-to-list 'emms-info-functions 'emms-info-mpd)
 (add-to-list 'emms-player-list 'emms-player-mpd)
-(emms-player-set emms-player-mpd 'regex
-                 "\\.ogg\\|\\.mp3\\|\\.wma\\|\\.ogm\\|\\.asf\\|\\.mkv\\|http://\\|mms://\\|\\.rmvb\\|\\.flac\\|\\.vob\\|\\.m4a\\|\\.ape\\|\\.mpc\\|\\.MP3\\|\\.OGG\\|\\.FLAC")
+;; (emms-player-set emms-player-mpd 'regex
+;;                  "\\.ogg\\|\\.mp3\\|\\.wma\\|\\.ogm\\|\\.asf\\|\\.mkv\\|http://\\|mms://\\|\\.rmvb\\|\\.flac\\|\\.vob\\|\\.m4a\\|\\.ape\\|\\.mpc\\|\\.MP3\\|\\.OGG\\|\\.FLAC")
+
+;; (setq emms-player-mpd-supported-regexp
+;; 	  (concat "\\`http://\\|"
+;; 		  (emms-player-simple-regexp
+;; 		   "m3u" "ogg" "flac" "mp3" "wav" "mod" "au" "aiff")))
+    
+;;     (emms-player-set emms-player-mpd
+;; 		     'regex
+;; 		     emms-player-mpd-supported-regexp)
 (setq emms-player-mpd-music-directory "/var/lib/mpd/music/")
-(emms-cache 1)
-(setq emms-cache-file "~/.emacs.d/emms/cache")
-;; (setq emms-source-file-default-directory "/srv/multimedia/musique/")
 
-(emms-cache-set-from-mpd-all)
+(setq emms-source-file-default-directory "/var/lib/mpd/music/")
 
+;; (emms-cache-set-from-mpd-all)
+;; (emms-player-mpd-update-all)
+;; (emms-player-mpd-update-all-reset-cache)
 (setq emms-browser--covers-filename '(("cover_small.jpg" "cover_small.jpeg" "cover_small.png" "cover_small.gif" "cover_small.bmp")
  ("cover_med.jpg" "cover_med.jpeg" "cover_med.png" "cover_med.gif" "cover_med.bmp")
  ("cover_large.jpg" "cover_large.jpeg" "cover_large.png" "cover_large.gif" "cover_large.bmp")))
-(require 'emms-browser)
+
 ;; (emms-browser)
 
