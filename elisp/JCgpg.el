39e804c2cdcec91be03a4de601690c192289098c
Julien Cubizolles <j.cubizolles@free.fr>
2016-04-27 21:38:06 +0200

    JCgpg.el creation

---
+++ elisp/JCgpg.el
@@ -0,0 +1,7 @@
+;; ~/.emacs.d/JCgpg.el -*- mode: lisp-*-
+
+
+;; only query user if -*- epa-file-encrypt-to: ("j.cubizolles@free.fr"); -*- is absent 
+;; "Julien Cubizolles" is working too, maybe ("Julien Cubizolles")
+(setq epa-file-select-keys nil) 
+                                
