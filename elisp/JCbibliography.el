1f14678a20240628d9f3e09ebb0907d35a00b597
Julien Cubizolles <j.cubizolles@free.fr>
2016-05-10 10:26:17 +0200

    variables name changes for bibtex

--- elisp/JCbibliography.el
+++ elisp/JCbibliography.el
@@ -1,7 +1,14 @@
 ;;~/.emacs.d/JCbibliography.el -*- mode: lisp-*-
 
+(setq reftex-default-bibliography '("~/enseignement/papiers/bibliography.bib"))
+
+;; see org-ref for use of these variables
+(setq org-ref-bibliography-notes "~/enseignement/papiers/notes.org"
+      org-ref-default-bibliography '("~/enseignement/papiers/bibliography.bib")
+      org-ref-pdf-directory "~/enseignement/papiers/")
+
 (eval-after-load 'helm
   (lambda ()
     (require 'helm-bibtex)
-    (setq helm-bibtex-bibliography '("/home/wilk/enseignement/papiers/bibliography.bib"))
-    (setq helm-bibtex-library-path '("/home/wilk/enseignement/papiers/"))))
+    (setq bibtex-completion-bibliography "~/enseignement/papiers/bibliography.bib")
+    (setq bibtex-completion-library-path "~/home/wilk/enseignement/papiers/")))
