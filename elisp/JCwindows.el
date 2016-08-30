27111ef168ecc0696ae59c5fc8734c3449d537b5
Julien Cubizolles <j.cubizolles@free.fr>
2016-08-30 21:35:01 +0200

    C-x n bound to their switch-window versions

--- elisp/JCwindows.el
+++ elisp/JCwindows.el
@@ -5,5 +5,9 @@
 ;; (setq win-switch-idle-time 1.5)
 
 (global-set-key (kbd "C-x o") 'switch-window)
+(global-set-key (kbd "C-x 1") 'switch-window-then-maximize)
+(global-set-key (kbd "C-x 2") 'switch-window-then-split-below)
+(global-set-key (kbd "C-x 3") 'switch-window-then-split-right)
+(global-set-key (kbd "C-x 0") 'switch-window-then-delete)
 (setq switch-window-shortcut-style 'qwerty)
 (setq switch-window-qwerty-shortcuts '("q" "s" "d" "f" "j" "k" "l" "m" "a" "z" "i" "o"))
