1073edb6edd629beaf2bf92a73987cd553d67856
Julien Cubizolles <j.cubizolles@free.fr>
2016-09-17 18:19:16 +0200

    spaceline

---
+++ elisp/JCspaceline.el
@@ -0,0 +1,7 @@
+;; ~/.emacs.d/JCspaceline.el -*- mode: lisp-*-
+
+(require 'spaceline-config)
+(setq powerline-default-separator 'wave)
+(spaceline-compile)
+(spaceline-emacs-theme)
+(spaceline-helm-mode)
