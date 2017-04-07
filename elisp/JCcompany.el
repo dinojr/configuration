26b411c526e86b533fdae25ab05fd2ac42de717f
Julien Cubizolles <j.cubizolles@free.fr>
2017-04-07 11:24:35 +0200

    Cleaning of JCcompany.el

--- elisp/JCcompany.el
+++ elisp/JCcompany.el
@@ -1,9 +1,8 @@
 ;; ~/.emacs.d/JCcompany.el -*- mode: lisp-*-
 
-(eval-after-load 'company
-  '(progn
-     (define-key company-mode-map (kbd "C-:") 'helm-company)
-     (define-key company-active-map (kbd "C-:") 'helm-company)))
+(with-eval-after-load 'company
+  (define-key company-mode-map (kbd "C-:") 'helm-company)
+  (define-key company-active-map (kbd "C-:") 'helm-company))
 
 (add-hook 'after-init-hook 'global-company-mode)
 (company-auctex-init)
