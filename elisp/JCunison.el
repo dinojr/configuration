6c7100e6aa72c30195e2388d190967b540d8971f
Julien Cubizolles <j.cubizolles@free.fr>
2017-04-08 13:15:59 +0200

    JCunison.el defines M-x unison

---
+++ elisp/JCunison.el
@@ -0,0 +1,7 @@
+;; ~/.emacs.d/JCunison.el -*- mode: lisp-*-
+
+(require 'unison)
+(require 'unison-mode)
+
+(setq unison-program "/home/wilk/bin/unison")
+(setq unison-args '("-batch" "JoseyWales"))
