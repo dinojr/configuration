6428a7023f1a2d91ee8689c58455f33b61f62be0
Julien Cubizolles <j.cubizolles@free.fr>
2017-04-09 13:51:03 +0200

    fixes for running/stopping offlineimap from gnus

--- elisp/JCofflineimap.el
+++ elisp/JCofflineimap.el
@@ -1,11 +1,17 @@
 ;; ~/.emacs.d/JCofflineimap.el -*- mode: lisp-*-
 
-(with-eval-after-load 'gnus
-  (add-hook 'gnus-before-startup-hook
+(require 'gnus)
+(add-hook 'gnus-before-startup-hook
 	  (lambda ()
+	    (when (not (process-live-p (get-process "offlineimap")))
 	    (when (y-or-n-p "Start OfflineImap ? ")
-	      (offlineimap))))
-  (add-hook 'gnus-exit-gnus-hook
+	      (offlineimap)))))
+(add-hook 'gnus-before-resume-hook
+	  (lambda ()
+	    (when (not (process-live-p (get-process "offlineimap")))
+	    (when (y-or-n-p "Start OfflineImap ? ")
+	      (offlineimap)))))
+(add-hook 'gnus-exit-gnus-hook
 	  (lambda ()
 	    (when (process-live-p (get-process "offlineimap"))
 	      (when (y-or-n-p "Kill OfflineImap ? ")
