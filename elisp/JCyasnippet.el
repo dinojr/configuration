a90fb56283936275fec05bd4737ae51750c06bb7
Julien Cubizolles <j.cubizolles@free.fr>
2017-04-09 13:54:09 +0200

    yasnippet no longer reindents

--- elisp/JCyasnippet.el
+++ elisp/JCyasnippet.el
@@ -3,5 +3,7 @@
 (require 'yasnippet) ;; not yasnippet-bundle
 (yas-global-mode 1)
 (setq yas-triggers-in-field t); Enable nested triggering of snippets
-
+(setq yas-indent-line nil)
+(add-to-list 'sp-no-reindent-after-kill-modes 'latex-mode)
 ;; Raccourcis
+				
