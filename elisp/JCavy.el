63593371ba5bb41228a7c84da5b3e695a3eb6cb4
Julien Cubizolles <j.cubizolles@free.fr>
2017-04-26 23:15:49 +0200

    JCavy.el creation

---
+++ elisp/JCavy.el
@@ -0,0 +1,9 @@
+;; ~/.emacs.d/JCavy.el -*- mode: lisp-*-
+(require 'avy)
+(avy-setup-default)
+(global-set-key (kbd "C-c j") 'avy-goto-char)
+(global-set-key (kbd "C-c k") 'avy-goto-char-2)
+(global-set-key (kbd "C-c l") 'avy-goto-word-1)
+(global-set-key (kbd "C-c ù") 'avy-goto-line)
+
+
