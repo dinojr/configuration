c889781d7f5352837985d1a2a61587c3e828866d
Julien Cubizolles <j.cubizolles@free.fr>
2017-04-29 21:34:58 +0200

    cleanup of offlineimap configuration

--- elisp/JCofflineimap.el
+++ elisp/JCofflineimap.el
@@ -1,18 +1,33 @@
 ;; ~/.emacs.d/JCofflineimap.el -*- mode: lisp-*-
 
 (require 'gnus)
+
+(defun jc-ask-start-offlineimap ()
+  (when (not (process-live-p (get-process "offlineimap")))
+    (when (y-or-n-p "Start OfflineImap ? ")
+      (offlineimap))))
+
+(defun jc-ask-kill-offlineimap ()
+  (when (process-live-p (get-process "offlineimap"))
+    (when (y-or-n-p "Kill OfflineImap ? ")
+      (offlineimap-kill))))
+
+(defun jc-kill-offlineimap ()
+  (when (process-live-p (get-process "offlineimap"))
+    (offlineimap-kill)))
+  
 (add-hook 'gnus-before-startup-hook
-	  (lambda ()
-	    (when (not (process-live-p (get-process "offlineimap")))
-	    (when (y-or-n-p "Start OfflineImap ? ")
-	      (offlineimap)))))
+	  'jc-ask-start-offlineimap)
+
 (add-hook 'gnus-before-resume-hook
-	  (lambda ()
-	    (when (not (process-live-p (get-process "offlineimap")))
-	    (when (y-or-n-p "Start OfflineImap ? ")
-	      (offlineimap)))))
+	  'jc-ask-start-offlineimap)
+
 (add-hook 'gnus-exit-gnus-hook
-	  (lambda ()
-	    (when (process-live-p (get-process "offlineimap"))
-	      (when (y-or-n-p "Kill OfflineImap ? ")
-		(offlineimap-kill)))))
+	  'jc-ask-kill-offlineimap)
+
+(add-hook 'kill-emacs-hook
+	  'jc-kill-offlineimap)
+
+(add-hook 'gnus-suspend-gnus-hook
+	  'jc-kill-offlineimap)
+
