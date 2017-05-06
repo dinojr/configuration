f0a8e3b45b556e8eaf6f9a7331bc621c14d4f97e
Julien Cubizolles <j.cubizolles@free.fr>
2017-05-06 23:25:03 +0200

    key changes for avy: conflict with org-store-link

--- elisp/JCavy.el
+++ elisp/JCavy.el
@@ -3,7 +3,7 @@
 (avy-setup-default)
 (global-set-key (kbd "C-c j") 'avy-goto-char)
 (global-set-key (kbd "C-c k") 'avy-goto-char-2)
-(global-set-key (kbd "C-c l") 'avy-goto-word-1)
-(global-set-key (kbd "C-c ù") 'avy-goto-line)
+(global-set-key (kbd "C-c u") 'avy-goto-word-1)
+(global-set-key (kbd "C-c i") 'avy-goto-line)
 
 
