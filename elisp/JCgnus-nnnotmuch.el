f1c7d41f538e111621fc92fb55bd5d7bea766b55
Julien Cubizolles <j.cubizolles@free.fr>
2017-08-23 14:36:01 +0200

    nnnotmuch.el, not in use ATM

---
+++ elisp/JCgnus-nnnotmuch.el
@@ -0,0 +1,13 @@
+;; ~/.emacs.d/JCgnus-nnnnotmuch.el -*- mode: lisp-*-
+(add-to-list 'load-path "~/git-repositories/nnnotmuch/")
+(require 'nnnotmuch)
+
+(push '(nnnotmuch "") gnus-secondary-select-methods)
+
+(setq nnnotmuch-groups
+      '((""   ; Nameless server (the default Notmuch config)
+         ("recent" "date:7days..")
+         )
+        ))
+
+(setq nnnotmuch-program "notmuch") ; This is the default.
