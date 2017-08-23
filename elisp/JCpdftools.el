2f9a40daef683ead4187b910b335707181ce40c9
Julien Cubizolles <j.cubizolles@free.fr>
2017-08-23 14:47:35 +0200

    use lpr-command for pdftools
    can't choose the options on the fly though

--- elisp/JCpdftools.el
+++ elisp/JCpdftools.el
@@ -7,9 +7,14 @@
   (add-to-list 'org-file-apps '("\\.pdf::\\([[:digit:]]+\\)\\'" . (lambda (file link) (org-pdfview-open file))))
   )
 			      
-
 (setq revert-without-query (quote (".*.pdf")))
 
 (setq pdf-view-midnight-colors '("#f8f8f2" . "#282a36"))
 
 (add-hook 'pdf-view-mode-hook 'turn-on-auto-revert-mode 'pdf-view-midnight-minor-mode)
+
+(setq pdf-misc-print-programm-args `("-o landscape" "-o sides=two-sided-short-edge" "-o page-ranges=1-" "-P"  ,printer-name))
+
+;; (setq pdf-misc-print-programm-args `(,lpr-switches ,printer-name))
+
+(setq pdf-misc-print-programm lpr-command)
