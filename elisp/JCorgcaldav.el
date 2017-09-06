fb0f16d1d30077d7929bac51d7b5b585d58d0212
Julien Cubizolles <j.cubizolles@free.fr>
2017-09-06 22:14:38 +0200

    usage precision for org-caldav

--- elisp/JCorgcaldav.el
+++ elisp/JCorgcaldav.el
@@ -1,11 +1,22 @@
 ;; ~/.emacs.d/JCorgcaldav.el -*- mode: lisp-*-
 (require 'org-caldav)
-(setq org-caldav-url "https://www.google.com/calendar/dav")
+
+
+(setq org-caldav-url 'google)
+
+;; Projet Org-mode créé sur https://console.developers.google.com/projectselector/apis/credentials
+;; Il faut aussi, dans "Tableau de bord", activer l'API CalDAV
+(setq org-caldav-oauth2-client-id "760965627563-fsbk1a5kddifiv5clrncuahdcc1n6tlc.apps.googleusercontent.com")
+(setq org-caldav-oauth2-client-secret "6eJFJObg_C4v7K4_dfEQCnDS")
 (setq org-caldav-calendar-id "09qqpld5o29tddmmv92dse1tng@group.calendar.google.com")
+
+(setq plstore-cache-passphrase-for-symmetric-encryption t)
+
 (setq org-caldav-inbox "~/org/org-caldav.org")
 (setq org-caldav-files '("~/org/orgfiles/planning-17-18.org" "~/org/orgfiles/lycee-17-18.org"))
 (setq org-icalendar-timezone "Europe/Paris")
 (setq org-caldav-save-directory "~/org/") ; to ensure synchronization by unison
+
 ;; Fonction pour créer plein d'entrées 
 ;; #+begin_src emacs-lisp :results raw
 ;; (with-temp-buffer
@@ -29,3 +40,7 @@
 
 
 (global-set-key [f5] 'org-caldav-sync)
+
+
+;; Il ne faut pas lancer org-caldav-sync depuis un emacsclient sinon
+;; le pinentry ne peut pas se lancer
