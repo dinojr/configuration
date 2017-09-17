2dc8396ab251752f8f1d65e6ba4cba1b86c1c9de
Julien Cubizolles <j.cubizolles@free.fr>
2017-09-17 23:00:54 +0200

    JCgpg.el: epa-pinentry-mode set to loopback: needed to use pinentry-emacs

--- elisp/JCgpg.el
+++ elisp/JCgpg.el
@@ -4,4 +4,5 @@
 ;; only query user if -*- epa-file-encrypt-to: ("j.cubizolles@free.fr"); -*- is absent 
 ;; "Julien Cubizolles" is working too, maybe ("Julien Cubizolles")
 (setq epa-file-select-keys nil) 
-                                
+
+(setq epa-pinentry-mode 'loopback)
