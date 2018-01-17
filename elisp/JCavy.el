d937a2786dec85254b95963b7ed27ead4dd09226
Julien Cubizolles <j.cubizolles@free.fr>
2018-01-17 13:32:49 +0100

    ace-isearch

--- elisp/JCavy.el
+++ elisp/JCavy.el
@@ -6,13 +6,13 @@
 (global-set-key (kbd "C-c u") 'avy-goto-word-1)
 (global-set-key (kbd "C-c i") 'avy-goto-line)
 
-;; (require 'ace-isearch)
-;; (global-ace-isearch-mode +1)
+(require 'ace-isearch)
+(global-ace-isearch-mode 1)
 
-;; (custom-set-variables
-;;  '(ace-isearch-input-length 7)
-;;  '(ace-isearch-jump-delay 0.25)
-;;  '(ace-isearch-function 'avy-goto-char)
-;;  '(ace-isearch-use-jump 'printing-char))
+(custom-set-variables
+ '(ace-isearch-input-length 5)
+ '(ace-isearch-jump-delay 0.25)
+ '(ace-isearch-function 'avy-goto-word-1)
+ '(ace-isearch-use-jump 'printing-char))
  
-;; (define-key isearch-mode-map (kbd "C-'") 'ace-isearch-jump-during-isearch)q
+(define-key isearch-mode-map (kbd "C-'") 'ace-isearch-jump-during-isearch)
