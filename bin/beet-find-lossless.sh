e5f4221c40482752607103b831de5383d9d5cc99
Julien Cubizolles <j.cubizolles@free.fr>
2018-04-28 15:31:55 +0200

    beet-configuration updates

--- bin/beet-find-lossless.sh
+++ bin/beet-find-lossless.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR=/srv/multimedia/musique/ADMINISTRATION/
+#export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.6/dist-packages/
 FILE=beets-lossless.txt
 date > $DIR/$FILE
-beet list "format:flac" -f '$albumartist - $album - $format' |uniq >> $DIR/$FILE
-
+beet list -a albumformat:FLAC -f '$albumartist - $album - $albumformat' >> $DIR/$FILE
--- bin/beet-find-lossy.sh
+++ bin/beet-find-lossy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DIR=/srv/multimedia/musique/ADMINISTRATION/
+#export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.6/dist-packages/
 FILE=beets-lossy.txt
 date > $DIR/$FILE
-beet list "format::(OGG|MP3)" -f '$albumartist - $album - $format' |uniq >> $DIR/$FILE
+beet list -a "albumformat::(MP3|OGG)" -f '$albumartist - $album - $albumformat' >> $DIR/$FILE
 
