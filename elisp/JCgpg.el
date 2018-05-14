134bd5e097b24e65b761bb6943741b2787274dbe
Julien Cubizolles <j.cubizolles@free.fr>
2018-05-14 17:01:38 +0200

    pinentry fix for emacsclient

--- elisp/JCgpg.el
+++ elisp/JCgpg.el
@@ -1,8 +1,21 @@
 ;; ~/.emacs.d/JCgpg.el -*- mode: lisp-*-
 
-
 ;; only query user if -*- epa-file-encrypt-to: ("j.cubizolles@free.fr"); -*- is absent 
 ;; "Julien Cubizolles" is working too, maybe ("Julien Cubizolles")
-(setq epa-file-select-keys nil) 
+(require 'epa-file)
+(epa-file-enable)
 
+;; https://www.emacswiki.org/emacs/EasyPG#toc5
+(defun jc/kludge-gpg-agent
+    ()
+  (if
+      (display-graphic-p)
+      (setenv "DISPLAY"
+              (terminal-name))
+    (setenv "GPG_TTY"
+            (terminal-name))
+    (setenv "DISPLAY")))
+
+(add-hook 'window-configuration-change-hook 'jc/kludge-gpg-agent)
+(setq epa-file-select-keys nil)
 (setq epa-pinentry-mode 'loopback)
