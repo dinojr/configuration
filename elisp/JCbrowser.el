f2e6fb5a3aa703e7234bf123bd25bff02dd302db
Julien Cubizolles <j.cubizolles@free.fr>
2018-08-12 08:33:11 +0200

    eww-lnum

--- elisp/JCbrowser.el
+++ elisp/JCbrowser.el
@@ -1,6 +1,12 @@
 ;; ~/.emacs.d/JCbrowser.el -*- mode: lisp-*-
 (require 'eww)
 
+(use-package eww-lnum
+  :after eww
+  :bind (:map eww-mode-map
+              ("f" . eww-lnum-follow)
+              ("F" . eww-lnum-universal)))
+
 (defun choose-browser (url &rest args)
   (interactive "sURL: ")
   (if (y-or-n-p "Use external browser? ")
