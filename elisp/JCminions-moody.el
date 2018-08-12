6db17137edaaf11fe990ec5ac09003b3a64bb811
Julien Cubizolles <j.cubizolles@free.fr>
2018-08-12 10:43:46 +0200

    JCminions-moody.el

---
+++ elisp/JCminions-moody.el
@@ -0,0 +1,8 @@
+;; (use-package moody
+;;   :config
+;;   (setq x-underline-at-descent-line t)
+;;   (moody-replace-mode-line-buffer-identification)
+;;   (moody-replace-vc-mode))
+
+(use-package minions
+  :config (minions-mode 1))
