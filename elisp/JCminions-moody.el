22dcadeb789e045cd3107b768bb35641c32d04f5
Julien Cubizolles <j.cubizolles@free.fr>
2018-11-04 09:15:04 +0100

    minions & moody configuration

--- elisp/JCminions-moody.el
+++ elisp/JCminions-moody.el
@@ -1,8 +1,36 @@
-;; (use-package moody
-;;   :config
-;;   (setq x-underline-at-descent-line t)
-;;   (moody-replace-mode-line-buffer-identification)
-;;   (moody-replace-vc-mode))
+;; ~/.emacs.d/JCminions-moody.el -*- mode: lisp-*-
+(require 'package)
+(package-initialize)
+(setq sml/theme 'dark)
+(smart-mode-line-enable)
+(use-package moody
+  :config
+  (setq x-underline-at-descent-line t)
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode))
+
+(moody-replace-sml/mode-line-buffer-identification)
 
 (use-package minions
-  :config (minions-mode 1))
+  :ensure t
+  :init (minions-mode)
+  :config
+  (setq
+   minions-mode-line-lighter "#"
+   minions-direct '(flycheck-mode
+		    mingus-mode-line-objet)
+   ))
+
+
+;; (require 'xpm) ;; for major-mode-icons
+;; (use-package major-mode-icons
+;;   :ensure t
+;;   :config
+;;   (major-mode-icons-mode 1))
+
+;; (setq
+;;  mode-line-end-spaces
+;;  (quote
+;;   ((:eval
+;;     (major-mode-icons/show))
+;;     )))
