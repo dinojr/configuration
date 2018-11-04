92bed67dc73d27873785c2513a7293faaebcddea
Julien Cubizolles <j.cubizolles@free.fr>
2018-11-04 09:08:49 +0100

    JCeshell: PATH bugfix

--- elisp/JCeshell.el
+++ elisp/JCeshell.el
@@ -1,3 +1,3 @@
 ;;~/.emacs.d/JCeshell.el -*- mode: lisp-*-
 
-(eshell/addpath "-b" "~/bin/" "/usr/local/texlive/current/bin/x86_64-linux")
+;; (eshell/addpath "-b" "~/bin/" "/usr/local/texlive/current/bin/x86_64-linux")
