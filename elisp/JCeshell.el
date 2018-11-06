0ba9bb18796393fc0a55bbd5ffef912dd8ab8c7a
Julien Cubizolles <j.cubizolles@free.fr>
2018-11-06 11:31:30 +0100

    path configuration in emacs

--- elisp/JCeshell.el
+++ elisp/JCeshell.el
@@ -1,3 +1,6 @@
 ;;~/.emacs.d/JCeshell.el -*- mode: lisp-*-
 
 ;; (eshell/addpath "-b" "~/bin/" "/usr/local/texlive/current/bin/x86_64-linux")
+
+(setq exec-path (append exec-path '("~/bin/" "/usr/local/texlive/current/bin/x86_64-linux/")))
+(setenv "PATH" (concat (getenv "PATH") ":~/bin/" ":/usr/local/texlive/current/bin/x86_64-linux/"))
