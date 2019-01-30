d6739b61676de4e85a597ba45ce31f1caa0d1120
Julien Cubizolles <j.cubizolles@free.fr>
2019-01-30 14:50:25 +0100

    JCcalendrier cleaning for theme loading

--- elisp/JCcalendrier.el
+++ elisp/JCcalendrier.el
@@ -43,16 +43,27 @@
                             (progn
                               (select-frame frame)
 			      (copy-face font-lock-constant-face 'calendar-iso-week-face)
+			      (set-face-attribute 'calendar-iso-week-face nil :height 0.7)
+			      (setq calendar-intermonth-text
+				    '(propertize
+				      (format "%2d"
+					      ;; le +10 est à ajuster à chaque vacances pour avoir les semaines de colles
+					      (+ 10 (car 
+						     (calendar-iso-from-absolute
+						      (calendar-absolute-from-gregorian (list month day year))))))
+				      'font-lock-face 'calendar-iso-week-face))
                               ))))
-  (copy-face font-lock-constant-face 'calendar-iso-week-face))
-
-(set-face-attribute 'calendar-iso-week-face nil
-                    :height 0.7)
-(setq calendar-intermonth-text
+  (progn (copy-face font-lock-constant-face 'calendar-iso-week-face)
+  (set-face-attribute 'calendar-iso-week-face nil :height 0.7)
+  (setq calendar-intermonth-text
       '(propertize
         (format "%2d"
 		;; le +10 est à ajuster à chaque vacances pour avoir les semaines de colles
                 (+ 10 (car 
                  (calendar-iso-from-absolute
                   (calendar-absolute-from-gregorian (list month day year))))))
-        'font-lock-face 'calendar-iso-week-face))
+        'font-lock-face 'calendar-iso-week-face))))
+  
+
+
+
