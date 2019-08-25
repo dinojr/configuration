e67262482340552c6bf82ee33256b602a9c8576e
Julien Cubizolles <j.cubizolles@free.fr>
2019-08-25 09:20:28 +0200

    emacsclient service stop action now runs some non interactive hooks

--- elisp/JCdesktop.el
+++ elisp/JCdesktop.el
@@ -2,3 +2,13 @@
 
 (require 'desktop)
 (desktop-save-mode 1)
+
+(if (daemonp)
+    (progn
+      (setq desktop-save 'if-exists)
+      ())
+  (setq desktop-save 'ask-if-exists))
+
+
+(setq daemon-kill-emacs-hook
+      '(recentf-save-list tex-delete-last-temp-files flycheck-global-teardown desktop-kill org-id-locations-save reftex-kill-emacs-hook bookmark-exit-hook-internal org-clock-save transient-maybe-save-history org-babel-remove-temporary-directory ps-kill-emacs-check server-force-stop))
