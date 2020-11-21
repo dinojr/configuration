97f89cc28c4a729086dc40646fda40081699e2ed
Julien Cubizolles <j.cubizolles@free.fr>
2020-11-21 15:41:44 +0100

    creation of -small version with gs

--- bin/jc-rearrange-A3scan.sh
+++ bin/jc-rearrange-A3scan.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 #A3 scans should be in the ordre 2 3 4 1, gives an A4 document in the right order
 PDF=$1
-NOM=${PDF/.pdf/-split.pdf}
+PDFSMALL=${PDF/.pdf/-small.pdf}
 SPLIT="split.pdf"
 ONE="1.pdf"
 TWO="2.pdf"
@@ -9,6 +9,7 @@ THREE="3.pdf"
 FOUR="4.pdf"
 TWOFOUR="24.pdf"
 THREEONE="31.pdf"
+BIG="big.pdf"
 mutool poster -x2  $PDF $SPLIT
 # order is 2 3 4 1
 pdftk A=$SPLIT cat Aodd output $TWOFOUR
@@ -18,8 +19,9 @@ pdftk A=$TWOFOUR cat Aodd output $TWO
 pdftk A=$THREEONE cat Aodd output $THREE
 pdftk A=$THREEONE cat Aeven output $ONE
 
-pdftk A=$ONE B=$TWO C=$THREE D=$FOUR shuffle A B C D output $NOM
+pdftk A=$ONE B=$TWO C=$THREE D=$FOUR shuffle A B C D output $BIG
+gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -sOutputFile=$PDFSMALL $BIG
 
-for i in $SPLIT $TWOFOUR $THREEONE $FOUR $THREE $TWO $ONE
+for i in $SPLIT $TWOFOUR $THREEONE $FOUR $THREE $TWO $ONE $BIG
 	do rm $i
 done
