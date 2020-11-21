6354da11965bdd4e5dc247e98cf8bbce97c54adc
Julien Cubizolles <j.cubizolles@free.fr>
2020-11-21 15:40:52 +0100

    order error fix

--- bin/jc-rearrange-A3scan.sh
+++ bin/jc-rearrange-A3scan.sh
@@ -7,15 +7,15 @@ ONE="1.pdf"
 TWO="2.pdf"
 THREE="3.pdf"
 FOUR="4.pdf"
-FOURTHREE="43.pdf"
-TWOONE="21.pdf"
+TWOFOUR="24.pdf"
+THREEONE="31.pdf"
 mutool poster -x2  $PDF $SPLIT
 # order is 2 3 4 1
 pdftk A=$SPLIT cat Aodd output $TWOFOUR
 pdftk A=$SPLIT cat Aeven output $THREEONE
-pdftk A=$TWOFOUR cat Aeven output $THREE
-pdftk A=$TWOFOUR cat Aodd output $FOUR
-pdftk A=$THREEONE cat Aodd output $TWO
+pdftk A=$TWOFOUR cat Aeven output $FOUR
+pdftk A=$TWOFOUR cat Aodd output $TWO
+pdftk A=$THREEONE cat Aodd output $THREE
 pdftk A=$THREEONE cat Aeven output $ONE
 
 pdftk A=$ONE B=$TWO C=$THREE D=$FOUR shuffle A B C D output $NOM
