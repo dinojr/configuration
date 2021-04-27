3a2961be621077a95c74f5300956d633b5391a92
Julien Cubizolles <j.cubizolles@free.fr>
2021-04-27 14:52:12 +0200

    helm-split-window-in-side-p set to nil

--- elisp/JChelm.el
+++ elisp/JChelm.el
@@ -32,10 +32,10 @@
 (require 'helm-org-rifle)
 (setq helm-org-rifle-show-path t)
 
-(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
-      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+(setq helm-split-window-in-side-p nil
+      helm-move-to-line-cycle-in-source t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount 8 ; scroll 8 lines other window using M-<next>/M-<prior>
       helm-ff-file-name-history-use-recentf t)
 
 (helm-autoresize-mode t)
