43617c9d9858f79bca5e02e243de8d194977f155
Julien Cubizolles <j.cubizolles@free.fr>
2021-08-13 08:03:58 +0200

    address in ac-paris.fr added to gnus-ignored-from-addresses

--- elisp/dotgnus.el
+++ elisp/dotgnus.el
@@ -108,7 +108,7 @@
 ;(setq nnimap-fetch-partial-articles "text/")
 (setq nnimap-fetch-partial-articles nil)
 
-(setq gnus-ignored-from-addresses (regexp-opt '("j.cubizolles@free.fr" "j.cubizolles@gmail.com")))
+(setq gnus-ignored-from-addresses (regexp-opt '("j.cubizolles@free.fr" "j.cubizolles@gmail.com" "julien.cubizolles@ac-paris.fr")))
 
 (setq bbdb/gnus-split-nomatch-function nil
       bbdb/gnus-split-default-group nil
