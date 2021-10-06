bbf57a584a4fa62f719142a0d3c69083aa608d17
Julien Cubizolles <j.cubizolles@free.fr>
2021-10-06 22:37:36 +0200

    new org-export-async-init-python.el

---
+++ elisp/org-export-async-init-python.el
@@ -0,0 +1,23 @@
+;;~/.emacs -*- mode: emacs-lisp-*-
+;; (setq org-export-async-debug t)
+;; si org est installé avec melpa
+;; (require 'package)
+;; (package-initialize)
+
+(require 'package) ;; org-ref is loaded through melpa
+(package-initialize)
+
+;; si org est installé avec git
+(add-to-list 'load-path "~/git-repositories/org-mode/lisp")
+(add-to-list 'load-path "~/git-repositories/org-mode/contrib/lisp")
+
+(require 'org)
+(require 'ox)
+(require 'notifications)
+
+(add-to-list 'load-path "~/configuration/elisp/")
+(load "JCorg.el")
+(load "JClatex.el")
+(load "JCtramp.el")
+
+(setq org-latex-title-command "")
