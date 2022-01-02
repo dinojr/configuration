a1080c6a52a3a8bbe8a999d6413f0668975ec75c
Julien Cubizolles <j.cubizolles@free.fr>
2022-01-02 21:49:45 +0100

    truncate-all for doom-modeline-buffer-file-name-style

--- elisp/JCmodeline.el
+++ elisp/JCmodeline.el
@@ -28,7 +28,8 @@
 ;; If you are expereicing the laggy issue, especially while editing remote files
 ;; with tramp, please try `file-name' style.
 ;; Please refer to https://github.com/bbatsov/projectile/issues/657.
-(setq doom-modeline-buffer-file-name-style 'truncate-upto-project)
+;; (setq doom-modeline-buffer-file-name-style 'truncate-upto-project)
+(setq doom-modeline-buffer-file-name-style 'truncate-all)
 
 ;; Whether display icons in mode-line or not.
 ;; (setq doom-modeline-icon (display-graphic-p))
