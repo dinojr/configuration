c7fc18fbbcf49b812fabd0ec13be762cf81203c5
Julien Cubizolles <j.cubizolles@free.fr>
2022-01-05 19:19:38 +0100

    "d" action in ediff-merge-files to combine "A" and "B" buffers

--- elisp/JCediff.el
+++ elisp/JCediff.el
@@ -1,3 +1,12 @@
 ;; ~/.emacs.d/general_stuff.el -*- mode: emacs-lisp-*-
 
 (setq ediff-merge-split-window-function 'split-window-vertically)
+
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
