a17257eeddb9ad8855a00606ed24cf60334276e6
Julien Cubizolles <j.cubizolles@free.fr>
2022-01-09 14:07:59 +0100

    exec-path configuration with exec-path-from-shell-initialize

--- elisp/JCeshell.el
+++ elisp/JCeshell.el
@@ -2,8 +2,10 @@
 
 ;; (eshell/addpath "-b" "~/bin/" "/usr/local/texlive/current/bin/x86_64-linux")
 
-(setq exec-path (append exec-path '("~/bin/" "/usr/local/texlive/current/bin/x86_64-linux/")))
-(setenv "PATH" (concat (getenv "PATH") ":~/bin/" ":/usr/local/texlive/current/bin/x86_64-linux/"))
+;; (setq exec-path (append exec-path '("~/bin/" "/usr/local/texlive/current/bin/x86_64-linux/")))
+;; (setenv "PATH" (concat (getenv "PATH") ":~/bin/" ":/usr/local/texlive/current/bin/x86_64-linux/"))
+
+(exec-path-from-shell-initialize)
 
 (use-package eshell-syntax-highlighting
   :after esh-mode
