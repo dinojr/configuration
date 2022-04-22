d95dad728733da6db21f0a86e87cda4c1794e215
Julien Cubizolles <j.cubizolles@free.fr>
2022-04-22 17:02:49 +0200

    batch processing des copies de TP

--- bin/jc-grille-TP.sh
+++ bin/jc-grille-TP.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 GRILLE_PDF="/home/wilk/enseignement/TP/grille-evaluation.pdf"
-FICHIER=$1
-FICHIER_GRILLE=${FICHIER/.pdf/-grille.pdf}
-echo "fichier $FICHIER"
-pdftk $GRILLE_PDF $FICHIER cat output $FICHIER_GRILLE
-rm $FICHIER
+GRILLE_SUFFIX="grille.pdf"
+DIR="grille"
+[ ! -d $DIR ] && mkdir $DIR
+
+for i in *.pdf
+do
+    FILENAME=$i
+    FILENAME_BASE="${i/.pdf/}"
+    FICHIER_GRILLE="$DIR/$FILENAME_BASE-$GRILLE_SUFFIX"
+    if [ ! -f "$FICHIER_GRILLE" ]
+    then echo "On grille la copie de $FILENAME_BASE"
+	 pdftk $GRILLE_PDF $FILENAME cat output $FICHIER_GRILLE
+	 rm $FILENAME
+    else echo "$FILENAME_BASE déjà fait"
+    fi
+done
+
 
