8dc3386953b0358f9b148559c3e4c6fcf776789f
Julien Cubizolles <j.cubizolles@free.fr>
2022-08-02 10:13:05 +0200

    jc-xournalpp-corr.sh: now displays the papers not yet graded

--- bin/jc-xournalpp-corr.sh
+++ bin/jc-xournalpp-corr.sh
@@ -2,16 +2,24 @@
 
 PDF_SUFFIX="corr.pdf"
 DIR="corr"
-
+XOPP_SUFFIX=".xopp"
+ACORRIGER="Reste à corriger: "
 [ ! -d $DIR ] && mkdir $DIR
-for i in *.xopp
+
+for i in *.pdf
 do
     FILENAME=$i
-    FILENAME_BASE="${i/.xopp/}"
-    PDF_FILENAME="$DIR/$FILENAME_BASE-$PDF_SUFFIX"
-    if [ ! -f "$PDF_FILENAME" ]
-    then echo "On pdfise la copie de $FILENAME_BASE"
-	 xournalpp -p "$PDF_FILENAME" "$FILENAME"
-    else echo "$FILENAME_BASE déjà fait"
+    FILENAME_BASE="${i/.pdf/}"
+    XOPP_FILENAME="$FILENAME_BASE$XOPP_SUFFIX"
+    if [ -f $XOPP_FILENAME ]
+    then PDF_FILENAME="$DIR/$FILENAME_BASE-$PDF_SUFFIX"
+	 if [ ! -f "$PDF_FILENAME" ]
+	 then echo "On pdfise la copie de $FILENAME_BASE"
+	      xournalpp -p "$PDF_FILENAME" "$XOPP_FILENAME"
+	 else echo "$FILENAME_BASE déjà fait"
+	 fi
+    else echo -e "fichier \e[31m$FILENAME\e[0m à corriger"
+	 ACORRIGER+="\n\e[31m$FILENAME\e[0m"
     fi
 done
+echo -e "$ACORRIGER"
