80d3bff33fe9d39e369da2d95526b60837d20cb5
Julien Cubizolles <j.cubizolles@free.fr>
2022-08-02 10:12:04 +0200

    all-the-icons-completion-mode

--- elisp/general_stuff.el
+++ elisp/general_stuff.el
@@ -90,7 +90,8 @@
 (use-package all-the-icons
   :config (lambda ()
 	    (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
-	    (add-hook 'gnus-started-hook 'all-the-icons-gnus-setup))
+	    (add-hook 'gnus-started-hook 'all-the-icons-gnus-setup)
+	    (all-the-icons-completion-mode))
   :ensure t)
 ;; M-x all-the-icons-install-fonts
 
