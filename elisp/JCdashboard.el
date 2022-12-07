373e205e50b96d2cd8d37ff78b9cc26c23705c42
Julien Cubizolles <j.cubizolles@free.fr>
2022-12-07 15:44:23 +0100

    dashboard configuration

---
+++ elisp/JCdashboard.el
@@ -0,0 +1,9 @@
+;; ~/.emacs.d/JCdashboard.el -*- mode: emacs-lisp-*-
+
+
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook))
+
+(setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
