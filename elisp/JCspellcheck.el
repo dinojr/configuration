cfd25d1fedee46400402fa92a9c0c27c830a5b68
Julien Cubizolles <j.cubizolles@free.fr>
2022-12-13 15:31:36 +0100

    spellcheck configuration through use-package

--- elisp/JCspellcheck.el
+++ elisp/JCspellcheck.el
@@ -1,25 +1,16 @@
 ;; ~/.emacs.d/JCspellcheck.el -*- mode: emacs-lisp-*-
 
-(setq flyspell-use-meta-tab nil)
-(setq ispell-program-name "aspell")
-(setq ispell-list-command "list")
-
-(add-hook 'text-mode-hook
-	  (lambda () (ispell-change-dictionary "francais"))
-	  )
-(setq ispell-tex-skip-alists
-      (list
-       (append (car ispell-tex-skip-alists)
-               '(("\\\\cite"            ispell-tex-arg-end)
-                 ("\\\\nocite"          ispell-tex-arg-end)
-                 ("\\\\includegraphics" ispell-tex-arg-end)
-                 ("\\\\figScale"         ispell-tex-arg-end)
-                 ("\\\\author"          ispell-tex-arg-end)
-                 ("\\\\ref"             ispell-tex-arg-end)
-                 ("\\\\eqref"             ispell-tex-arg-end)
-                 ("\\\\label"           ispell-tex-arg-end)
-                 ))
-       (cadr ispell-tex-skip-alists)))
+(use-package flyspell
+  :init
+  (flyspell-mode 1)
+  :bind (("C-c d" . ispell-change-dictionary)
+	 :map flyspell-mode-map
+	 ("C-;" . flyspell-correct-previous))
+  :config
+  (setq ispell-program-name "aspell") ;; run flyspell with aspell, not ispell
+  (setq flyspell-use-meta-tab nil)
+  :hook text-mode-hook ((lambda () (ispell-change-dictionary "francais")))
+  )
 
 ;; (defun jc-flyspell-check-next-highlighted-word ()
 ;;   "Custom function to spell check next highlighted word"
@@ -29,11 +20,10 @@
 ;;   )
 
 
-(ace-flyspell-setup) ; binds C-. to ace-flyspell-dwim
+;; (ace-flyspell-setup) ; binds C-. to ace-flyspell-dwim
 
 ;; Raccourcis
-(global-set-key "\C-cd" 'ispell-change-dictionary)
+
 ;; (global-set-key (kbd "M-<f8>") 'jc-flyspell-check-next-highlighted-word)
 
-(require 'flyspell-correct-helm)
-(define-key flyspell-mode-map (kbd "C-;") 'flyspell-correct-previous)
+
