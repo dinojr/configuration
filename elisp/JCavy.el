33a5551919c7c41f46b58a12e4e70ab297d7ed84
Julien Cubizolles <j.cubizolles@free.fr>
2023-01-16 17:24:04 +0100

    JCavy use-package refactoring

--- elisp/JCavy.el
+++ elisp/JCavy.el
@@ -1,7 +1,9 @@
 ;; ~/.emacs.d/JCavy.el -*- mode: emacs-lisp-*-
-(use-package avy)
-(avy-setup-default)
-(global-set-key (kbd "C-: j") 'avy-goto-char)
-(global-set-key (kbd "C-: k") 'avy-goto-char-2)
-(global-set-key (kbd "C-: u") 'avy-goto-word-1)
-(global-set-key (kbd "C-: i") 'avy-goto-line)
+(use-package avy
+  :config
+  (avy-setup-default)
+  (global-set-key (kbd "C-: j") 'avy-goto-char)
+  (global-set-key (kbd "C-: k") 'avy-goto-char-2)
+  (global-set-key (kbd "C-: u") 'avy-goto-word-1)
+  (global-set-key (kbd "C-: i") 'avy-goto-line))
+
