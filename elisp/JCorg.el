61680f9cf9ddd5ec5e868e77d819a5f12a2c7657
Julien Cubizolles <j.cubizolles@free.fr>
2023-02-28 13:07:32 +0100

    back to using org-checklist

--- elisp/JCorg.el
+++ elisp/JCorg.el
@@ -762,7 +762,7 @@ Skip over dangling clock entries."
 (setq org-ref-pdf-directory "~/enseignement/papiers")
 (setq org-ref-bibtex-files "~/enseignement/papiers/bibliography.bib")
 
-;; (require 'org-checklist) ; pour mettre à 0 les checkboxes quand une tâche est passée en DONE
+(require 'org-checklist) ; pour mettre à 0 les checkboxes quand une tâche est passée en DONE
 
 ; pour mettre des review sur une tâche
 (use-package org-review
