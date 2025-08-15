fcf14e38bcd7cba032ccce15fd2195eca365a55a
Julien Cubizolles <j.cubizolles@free.fr>
2025-08-15 19:03:40 +0200

    configuration de mbsync

--- elisp/JCmbsync.el
+++ elisp/JCmbsync.el
@@ -4,11 +4,20 @@
 
 ;; f runs the command mbsync
 (require 'mbsync)
-(add-hook 'mbsync-exit-hook 'gnus-group-get-new-news)
+;; (add-hook 'mbsync-exit-hook 'gnus-group-get-new-news)
 (add-hook 'mbsync-exit-hook 'notmuch-poll)
 (define-key gnus-group-mode-map (kbd "f") 'mbsync)
 
-(gnus-demon-add-handler 'mbsync 5 .5)
+
+
+;; (gnus-demon-add-handler 'mbsync 5 .5)
+
+
+(gnus-demon-add-handler 'jc-run-mbsync-if-nm-up 5 .5)
+
+(defun jc-run-mbsync-if-nm-up()
+  (when (nm-is-connected)
+    (mbsync)))
 
 (defun jc-ask-run-mbsync ()
     (when (y-or-n-p "Run mbsync ? ")
@@ -30,7 +39,7 @@
 	  'jc-ask-run-mbsync)
 
 (defun jc-notify-mbsync-exit ()
-  "Display a notification when a TeX run is finished."
+  "Display a notification when a mbsync run is finished."
   (notifications-notify :title "mbsync"
                         :body (format "Finished")))
 
