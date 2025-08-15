ad6c452f83f97676dfa9194a5d0379853ecb87aa
Julien Cubizolles <j.cubizolles@free.fr>
2025-08-15 19:07:58 +0200

    bugfix pour l'environnement graphique

--- elisp/JCpython.el
+++ elisp/JCpython.el
@@ -2,3 +2,9 @@
 
 (setenv "PYDEVD_DISABLE_FILE_VALIDATION" "1")
 (setq python-shell-interpreter "python3")
+
+;; matplotlib relies on $DISPLAY or $WAYLAND_DISPLAY to be set in order to use a GUI backend
+;; see https://github.com/matplotlib/matplotlib/issues/18377
+;; but an Emacs daemon started by systemd in wayland doesn't set these environemnt variables
+;; although they are set by an Emacs started from within the GNOME session
+(setenv "WAYLAND_DISPLAY" "wayland-0")
